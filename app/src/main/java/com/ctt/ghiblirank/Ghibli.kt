77a88d4926f5afdb883cee2153ddc7c89f491b6f
Pri Nitta <egashira.sayuri@gmail.com>
2021-03-06 10:16:37 -0300

    class finalized

--- app/src/main/java/com/ctt/ghiblirank/Ghibli.kt
+++ app/src/main/java/com/ctt/ghiblirank/Ghibli.kt
@@ -3,12 +3,18 @@ package com.ctt.ghiblirank
 import com.google.gson.annotations.SerializedName
 
 class Ghibli (
-    @SerializedName("Titulo em inglês")
-    @SerializedName("Nome original")
-    @SerializedName("Titulo em romaji")
-    @SerializedName("Descrição")
-    @SerializedName("Diretor")
-    @SerializedName("Ano")
+    @SerializedName("Titulo em inglês") val titulo: String,
+    @SerializedName("Nome original") val nomeJap: String,
+    @SerializedName("Titulo em romaji") val tituloRom: String,
+    @SerializedName("Descrição") val descricao:String,
+    @SerializedName("Diretor") val diretor: String,
+    @SerializedName("Ano") val ano: String
         ){
-    override fun toSting()
+    override fun toSting(): String{
+        return "Titulo: $titulo,\nNome Japonês: $nomeJap,\n" +
+                "Título em romaji: $tituloRom,\n" +
+                "Descrição: $descricao,\n" +
+                "Diretor: $diretor,\n" +
+                "Ano: $ano"
+    }
 }
