72f2e59fcbd12f203974bc13a7a4eaaf7c555d88
Pri Nitta <egashira.sayuri@gmail.com>
2021-03-06 10:30:24 -0300

    Network addition

---
+++ app/src/main/java/com/ctt/ghiblirank/Network.kt
@@ -0,0 +1,15 @@
+package com.ctt.ghiblirank
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class Network {
+    companion object{
+        fun RetrofitConfig(String): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl("https://ghibliapi.herokuapp.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+    }
+}
