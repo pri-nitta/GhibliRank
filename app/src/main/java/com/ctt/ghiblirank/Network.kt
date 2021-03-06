de72ca7dae00dee7ad5ec05735886af03574c60a
vanessasharine@gmail.com <vanessasharine@gmail.com>
2021-03-06 10:54:32 -0300

    Revisado bugs

--- app/src/main/java/com/ctt/ghiblirank/Network.kt
+++ app/src/main/java/com/ctt/ghiblirank/Network.kt
@@ -5,9 +5,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class Network {
     companion object{
-        fun RetrofitConfig(String): Retrofit {
+        fun RetrofitConfig(): Retrofit {
+        //fun RetrofitConfig(caminho: String): Retrofit {
             return Retrofit.Builder()
                 .baseUrl("https://ghibliapi.herokuapp.com")
+                //.baseUrl(caminho)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
         }
